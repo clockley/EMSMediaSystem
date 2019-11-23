6debce1298fd97509264ccdf369f18a6be04559b
Christian <clockley1@gmail.com>
2019-11-23 02:28:34 -0500

    don't style element and remove useless js fullscreen request

--- media.js
+++ media.js
@@ -3,13 +3,13 @@ var mediaFile = electron.remote.getCurrentWindow().webContents.browserWindowOpti
 
 function loadMedia() {
     var video = document.createElement('video');
+    video.setAttribute("id", "bigPlayer");
     video.src = mediaFile;
     video.setAttribute("controls", "controls");
     video.addEventListener("ended", function () {
         electron.remote.getCurrentWindow().close();
     });
     document.body.appendChild(video);
-    video.requestFullscreen();
     video.play();
 }
 
