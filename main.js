5b82c5e198660a3f8c6844e1f1f600a2eddfbd9f
Christian Lockley <clockley@users.noreply.github.com>
2022-12-06 21:31:27 -0500

    fix crash

--- main.js
+++ main.js
@@ -22,8 +22,8 @@ var toHHMMSS = (secs) => {
 
 function createWindow() {
   ipcMain.on('timeRemaining-message', (event, arg) => {
-    if (win.webContents == null) {
-      close()
+    if (win == null) {
+
     }
     win.webContents.send('timeRemaining-message', [toHHMMSS(arg[0] - arg[1]), (arg[1] / arg[0])*100+"%", arg[0], arg[1]])
   })
