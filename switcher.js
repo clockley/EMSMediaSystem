35ff3b92b5bf3fad1be955d8375ca7846a04ec81
Christian Lockley <7493440+clockley@users.noreply.github.com>
2024-05-18 08:17:52 -0400

    import atem switch code

--- switcher.js
+++ switcher.js
@@ -1,8 +1,8 @@
 const bonjour = require('bonjour')();
 const { Atem } = require('atem-connection');
-const atem = null;
+let atem = null;
 
-export function detectSwitcherAddress() {
+function detectSwitcherAddress() {
     bonjour.find({ type: '_blackmagic._tcp' }, function (service) {
         console.log('Found a Blackmagic device:');
         console.log(`Name: ${service.name}`);
@@ -14,22 +14,26 @@ export function detectSwitcherAddress() {
         bonjour.destroy();
         console.log('Stopped browsing');
     }, 30000);
-
-    return service.referer.address;
 }
 
-export async function connectToAtemSwitcher(ip) {
+async function connectToAtemSwitcher(ip) {
     atem = new Atem();
     atem.on('info', console.log);
     atem.on('error', console.error);
     await atem.connect(ip);
 }
 
-export function changeInput(num) {
+function changeInput(num) {
     if (!atem) {
         return;
     }
     atem.changeProgramInput(num).then(() => {
-        console.log('Program input changed to 1');
+        console.log('Program input changed to', num);
     }).catch(console.error);
 }
+
+module.exports = {
+    detectSwitcherAddress,
+    connectToAtemSwitcher,
+    changeInput
+};
