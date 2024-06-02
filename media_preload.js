371024687f44e79a4a9d7b36287412625851b1b4
Christian Lockley <7493440+clockley@users.noreply.github.com>
2024-06-01 23:56:01 -0400

    lock down media process

--- media_preload.js
+++ media_preload.js
@@ -4,13 +4,6 @@ contextBridge.exposeInMainWorld('electron', {
     ipcRenderer: {
         send: (channel, data) => ipcRenderer.send(channel, data),
         on: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(event, ...args)),
-        once: (channel, callback) => ipcRenderer.once(channel, (event, ...args) => callback(event, ...args)),
-        invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
-        sendSync: (channel, ...args) => ipcRenderer.sendSync(channel, ...args)
     },
-    argv: process.argv,
-    process: {
-        platform: process.platform,
-        versions: process.versions
-    }
+    argv: process.argv
 });
