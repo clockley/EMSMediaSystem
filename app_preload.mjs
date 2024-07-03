cd55d0a9429c509add0706ce56aa234c4641ff0c
Christian Lockley <7493440+clockley@users.noreply.github.com>
2024-07-03 08:44:10 -0400

    Revert "try highres clock again"
    This reverts commit 4279e4ef729ed77c20cb88a5fd7a36a1176a0f1c.

--- app_preload.mjs
+++ app_preload.mjs
@@ -5,19 +5,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Function to get high-resolution real time in milliseconds since process start
-const getHrtimeMs = () => {
-    const [seconds, nanoseconds] = process.hrtime();
-    return (seconds * 1000) + (nanoseconds / 1000000);
-};
-
-// Fetch the common reference time from the main process
-const referenceTimeMs = await ipcRenderer.invoke('get-reference-time');
-
-// Calculate the offset between system time and high-resolution time
-const hrtimeMs = getHrtimeMs();
-const offsetMs = referenceTimeMs - hrtimeMs;
-
 contextBridge.exposeInMainWorld('electron', {
     ipcRenderer: {
         send: (channel, data) => ipcRenderer.send(channel, data),
@@ -25,6 +12,5 @@ contextBridge.exposeInMainWorld('electron', {
         invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
     },
     path: path,
-    __dirname: __dirname,
-    getSynchronizedTime: () => getHrtimeMs() + offsetMs
+    __dirname: __dirname
 });
--- media_preload.js
+++ media_preload.js
@@ -1,25 +1,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Function to get high-resolution real time in milliseconds since process start
-const getHrtimeMs = () => {
-    const [seconds, nanoseconds] = process.hrtime();
-    return (seconds * 1000) + (nanoseconds / 1000000);
-};
-
-// Function to initialize and fetch the reference time asynchronously
-const initializeSynchronizedTime = async () => {
-    const referenceTimeMs = await ipcRenderer.invoke('get-reference-time');
-    const hrtimeMs = getHrtimeMs();
-    const offsetMs = referenceTimeMs - hrtimeMs;
-
-    contextBridge.exposeInMainWorld('electron', {
-        ipcRenderer: {
-            send: (channel, data) => ipcRenderer.send(channel, data),
-            on: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(event, ...args)),
-        },
-        argv: process.argv,
-        getSynchronizedTime: () => getHrtimeMs() + offsetMs,
-    });
-};
-
-initializeSynchronizedTime();
+contextBridge.exposeInMainWorld('electron', {
+    ipcRenderer: {
+        send: (channel, data) => ipcRenderer.send(channel, data),
+        on: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(event, ...args)),
+    },
+    argv: process.argv
+});
