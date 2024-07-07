5cc2996c41cbd4ff08422475e86e9565785e822f
Christian Lockley <7493440+clockley@users.noreply.github.com>
2024-07-06 21:20:44 -0400

    initial text presenter code

--- Bible.mjs
+++ Bible.mjs
@@ -1,8 +1,7 @@
 // Bible.js
 export class Bible {
     constructor() {
-        // Load the Go runtime
-        const go = new Go();
+          const go = new Go();
         WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
             go.run(result.instance);
         });
