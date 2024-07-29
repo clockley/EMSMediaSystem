3e8a481fb0f3c43efba8ea9914bd87d040ced67e
Christian Lockley <7493440+clockley@users.noreply.github.com>
2024-07-29 18:13:44 -0400

    remove //

--- main.mjs
+++ main.mjs
@@ -5,8 +5,6 @@ import path from 'path';
 
 let mediaWindow = null;
 let windowBounds;
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win = null;
 let ipcInitPromise = null;
 app.commandLine.appendSwitch('enable-experimental-web-platform-features', 'true');
@@ -84,14 +82,9 @@ async function createWindow() {
     win.on('resize', saveWindowBounds);
   });
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null;
     app.quit();
 
@@ -203,18 +196,13 @@ windowBounds = settings.get('windowBounds');
 
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
