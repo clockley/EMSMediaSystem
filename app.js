fe81ec5f12ce79bb7e694755e8a36714d92d1be7
Christian Lockley <7493440+clockley@users.noreply.github.com>
2024-11-09 17:29:12 -0500

    tune gain

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ class PIDController {
         this.lastError = 0;
         this.integral = 0;
         this.lastTimeDifference = 0;
-        this.kP = 0.015; // Proportional gain
-        this.kI = 0.003; // Integral gain
-        this.kD = 0.008; // Derivative gain
+        this.kP = 0.8; // Proportional gain
+        this.kI = 0.1; // Integral gain
+        this.kD = 0.15; // Derivative gain
         this.synchronizationThreshold = 0.01;
         this.lastUpdateTime = 0;
     }
