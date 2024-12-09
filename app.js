5e8abace289bc9ccad4b2565bcceeee164b3383a
Christian Lockley <7493440+clockley@users.noreply.github.com>
2024-12-09 16:11:19 -0500

    Move to the LGPLv3+ license

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-"use strict";
-//console.time("start");
+/*
+Copyright (C) 2019-2024 Christian Lockley
+This library is free software; you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 //Project Alchemy
-//Devel
-//Copyright 2019 - 2024 Christian Lockley
+
+"use strict";
 
 const { ipcRenderer, __dirname, bibleAPI, webUtils } = window.electron;
 
@@ -1961,7 +1975,7 @@ async function createMediaWindow() {
     if (pidController) {
         pidController.reset();
     }
-
+xx``
     if (video) {
         if (video.audioTracks && video.audioTracks[0]) {
             video.audioTracks[0].enabled = false;
