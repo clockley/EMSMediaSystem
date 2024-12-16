63b5a43a4ffd1fd147b77e339236688ede899c39
Christian Lockley <7493440+clockley@users.noreply.github.com>
2024-12-16 07:53:41 -0500

    move getHostnameOrBasename out of preload

--- app_preload.mjs
+++ app_preload.mjs
@@ -43,35 +43,6 @@ const [bibleAPI] = await Promise.all([
   executeWasmScript().then(() => initializeBible())
 ]);
 
-function getHostnameOrBasename(input) {
-  // Check if input contains a protocol-like prefix (http://, https://, ftp://, etc.)
-  const protocolMatch = input.match(/^(\w+):\/\//);
-
-  if (protocolMatch) {
-    // If protocol exists, extract hostname
-    const protocolEnd = protocolMatch[0].length;
-    const remainingPart = input.slice(protocolEnd);
-    const firstSlashIndex = remainingPart.indexOf('/');
-
-    // Return full domain or first part before path
-    return firstSlashIndex === -1
-      ? remainingPart
-      : remainingPart.slice(0, firstSlashIndex);
-  } else {
-    // If not a URL, extract basename
-    // Handle both forward and backslashes
-    const lastForwardSlash = input.lastIndexOf('/');
-    const lastBackSlash = input.lastIndexOf('\\');
-
-    // Choose the last separator
-    const lastSeparator = Math.max(lastForwardSlash, lastBackSlash);
-
-    // If no separator found, return the entire input
-    // Otherwise, return the part after the last separator
-    return lastSeparator === -1 ? input : input.slice(lastSeparator + 1);
-  }
-}
-
 contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: {
     send: ipcRenderer.send.bind(ipcRenderer),
@@ -81,8 +52,7 @@ contextBridge.exposeInMainWorld('electron', {
   },
   __dirname: import.meta.dirname,
   bibleAPI: bibleAPI,
-  webUtils: webUtils,
-  getHostnameOrBasename
+  webUtils: webUtils
 });
 
 contextBridge.exposeInMainWorld('windowControls', {
