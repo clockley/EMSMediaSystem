8eb60fa9119a13db078b4d5fd006677f91a227eb
Christian Lockley <7493440+clockley@users.noreply.github.com>
2025-02-05 15:28:05 -0500

    update css for themes and text module

--- main.mjs
+++ main.mjs
@@ -743,7 +743,7 @@ function createHelpWindow() {
       navigateOnDragDrop: false,
       spellcheck: false,
       preload: `${import.meta.dirname}/help_preload.mjs`,
-      devTools: false
+      devTools: true
     }
   });
 
